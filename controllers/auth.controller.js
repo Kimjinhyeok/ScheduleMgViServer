f723349b3f1d47390da282d405732300dc6ac33d
Kimjinhyeok <bkho2@naver.com>
2018-09-02 22:39:35 +0900

    JWT 생성 시, Promise를 사용해서 Callback과정을 수행후 Response를 리턴하는 수정

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -8,28 +8,36 @@ exports.authUserLogin = async function(req, res) {
 
     try{
         var promise = authService.authUserLogin(name, password);
-        promise.then( (resolve) =>  {
+        promise.then((resolve) =>  {
             //기한일(목표일)이 지난 일정을 비활성화 시킨다.
             scService.checkPassingDueDate(resolve.id);
-            jwt.sign({
-                userID : resolve.id,
-                userName : name
-            },
-            'abcd1234',
-            {
-                expiresIn : '7d',
-                subject : 'userInfo',
-                issuer : 'kjh'
-            },(err, res) => {
-                if(err){
-                    return res.status(400).json({
-                        err : err
-                    })
-                }else{
-                    return res.status(200).json({
-                        id : id
-                    })
-                }
+            var p = new Promise((resolve, reject) => {
+                jwt.sign({
+                    userID : resolve.id,
+                    userName : name
+                },
+                'abcd1234',
+                {
+                    expiresIn : '7d',
+                    subject : 'userInfo',
+                    issuer : 'kjh'
+                },(err, token) => {
+                    if(err){
+                        reject(err);
+                    }else{
+                        resolve(token);
+                    }
+                })
+            });
+            p.then(token => {      
+                return res.status(200).json({
+                    token : token
+                })
+            });
+            p.catch(reject => {
+                return res.status(400).json({
+                    err : reject.err
+                })
             })
         });
         promise.catch((err) => {
