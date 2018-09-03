61fbe00e0311ec52797f3b66f2589db04aa9daaf
Kimjinhyeok <bkho2@naver.com>
2018-09-03 22:32:58 +0900

    Authorization헤더가 CORS에 걸려서 허용 목록에 넣어줌

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ var app = express();
 // for Cross Origin Request
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "http://localhost:4200");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
   next();
 });
